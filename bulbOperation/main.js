afcf604a344b68f9307b4fefc0366cd77ee29081
Muizz Rafid <muizzrafid1@gmail.com>
2024-10-04 21:12:31 +0600

    bulb project completed

---
+++ bulbOperation/main.js
@@ -0,0 +1,15 @@
+var bulb = document.querySelector(".bulb");
+var btn = document.querySelector(".bulb-btn");
+
+var value = 0;
+btn.addEventListener("click", function () {
+  if (value == 0) {
+    bulb.style.backgroundColor = "yellow";
+    bulb.style.boxShadow = "0 0 25px 5px rgb(255, 255, 173)";
+    value = 1;
+  } else {
+    bulb.style.backgroundColor = "white";
+    bulb.style.boxShadow = "0 0 15px 2px rgb(219, 221, 222)";
+    value = 0;
+  }
+});
