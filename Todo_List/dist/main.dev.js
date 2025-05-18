7ac520f7d50d02e1e31c6f004cb9f0ae807ab369
Muizz Rafid <muizzrafid1@gmail.com>
2025-05-18 23:24:07 +0600

    todo list new project

---
+++ Todo_List/dist/main.dev.js
@@ -0,0 +1,135 @@
+"use strict";
+
+var todoInput = document.querySelector(".todo-input");
+var todoButton = document.querySelector(".todo-button");
+var todoList = document.querySelector(".todo-list");
+var filterOption = document.querySelector(".filter-todo");
+document.addEventListener("DOMContentLoaded", getLocalTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("change", filterTodo);
+
+function addTodo(event) {
+  event.preventDefault();
+  var todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  var newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo); //ADDING TO LOCAL STORAGE 
+
+  saveLocalTodos(todoInput.value);
+  var completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check-circle"></li>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  var trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></li>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  todoList.appendChild(todoDiv);
+  todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  var item = e.target;
+
+  if (item.classList[0] === "trash-btn") {
+    var todo = item.parentElement;
+    todo.classList.add("slide");
+    removeLocalTodos(todo);
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  if (item.classList[0] === "complete-btn") {
+    var _todo = item.parentElement;
+
+    _todo.classList.toggle("completed");
+  }
+}
+
+function filterTodo(e) {
+  var todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+
+        break;
+
+      case "incomplete":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+
+        break;
+    }
+  });
+}
+
+function saveLocalTodos(todo) {
+  var todos;
+
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getLocalTodos() {
+  var todos;
+
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  todos.forEach(function (todo) {
+    var todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    var newTodo = document.createElement("li");
+    newTodo.innerText = todo;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    var completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check-circle"></li>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    var trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></li>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    todoList.appendChild(todoDiv);
+  });
+}
+
+function removeLocalTodos(todo) {
+  var todos;
+
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  var todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
